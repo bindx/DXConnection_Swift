84c2eccad488b66fd3c624868149f78f3d7360c3
apple123 <apple123@macbook-air.local>
2015-12-01 11:20:53 +0800

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by apple on 15/12/1.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
